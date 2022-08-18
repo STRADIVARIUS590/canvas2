ebe5122b9bd90e1224cca1d800f8185dad114cf8
joel <joel.uss1728@gmail.com>
2022-08-18 10:48:38 -0500

    18-8-222

--- index.php
+++ index.php
@@ -26,6 +26,11 @@
         let canvas = document.getElementById('canvas');
         let ctx = canvas.getContext('2d');
         var color = 'red';
+        var fig = 'arc';
+        var press = false;
+
+        var superX = 0;
+        var superY = 0;
 
         // texto
 
@@ -76,7 +81,7 @@
         }); */
 
 
-        canvas.addEventListener('click',  (e)=>{
+    /*     canvas.addEventListener('click',  (e)=>{
          //   console.log(e.clientX, e.clientY);
             ctx.beginPath(); 
             ctx.strokeStyle = "#000000";
@@ -92,7 +97,7 @@
             console.log(color);
 
         });
-
+*//* 
         function generateRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
@@ -102,6 +107,73 @@
     
         return color + 'C9';
 }
+        canvas.addEventListener('mouseout', ()=>{
+            console.log(fig);
+            fig = (fig == 'arc') ? 'rec':'arc';
+        });
+
+        canvas.addEventListener('click',  (e)=>{
+            //   console.log(e.clientX, e.clientY);
+                ctx.beginPath(); 
+            
+                ctx.fillStyle = generateRandomColor();
+                if(fig == 'arc'){
+                    ctx.arc(e.offsetX, e.offsetY, 50,100, 0, Math.PI * 2);
+                    ctx.fill();
+                    ctx.stroke();
+                }else{
+                    ctx.rect(e.offsetX-25, e.offsetY-25, 50,50);
+                    ctx.fill();
+                    ctx.stroke();
+                }
+            });
+
+            canvas.addEventListener('mousemove', (e)=>{
+                if(press){
+                    ctx.fillStyle = '#000000'
+                    ctx.fillRect(e.offsetX-2.5, e.offsetY-2.5, 5,5);
+                    ctx.stroke();
+                }
+            });
+        
+        canvas.addEventListener('mousedown',(e)=>{
+            press = true;
+        });
+        canvas.addEventListener('mouseup',(e)=>{
+            press = false;
+        }); */
+
+
+        document.addEventListener('keydown',(e)=>{
+            console.log(e.keyCode);//w = 87, 
+            if(e.keyCode==87 || e.keyCode == 38){
+            //arriba
+                superY-=20;
+            } 
+            if(e.keyCode==40 || e.keyCode == 83){
+            //abajo
+                superY+=20;
+            } 
+            if(e.keyCode==37 || e.keyCode == 65){
+            //izq
+                superX-=20;
+            } 
+            if(e.keyCode==39 || e.keyCode == 68){
+            //der
+                superX+=20;
+            } 
+            paint();
+        });
+
+        function paint(){
+
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0,0,500,500);
+
+            ctx.fillStyle = 'red';
+            ctx.fillRect(superX, superY, 40,40);
+        }
+
 
 
 
